1a0571604b279a6febce52e842c21f52892084ab
Luis Correa <lcorrea@lcorrea-ltm.internal.salesforce.com>
2016-11-06 12:25:01 -0200

    New hello version

--- src/github.com/luiszerba/hello/hello.go
+++ src/github.com/luiszerba/hello/hello.go
@@ -8,6 +8,7 @@ import (
 func main(){
 	fmt.Printf("hello, world\n")
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("Soccoram me subi em um onbibus em marrocos"))
 	fmt.Printf("\n")
 }
 
