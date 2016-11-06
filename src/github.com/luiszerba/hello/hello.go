47a1a2c4873877d0afe72888a258bfb9dc2fe698
Luis Correa <lcorrea@lcorrea-ltm.internal.salesforce.com>
2016-11-06 01:01:47 -0200

    initial branch

---
+++ src/github.com/luiszerba/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/luiszerba/stringutil"
+)
+
+func main(){
+	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("\n")
+}
+
